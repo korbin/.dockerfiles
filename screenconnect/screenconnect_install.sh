823a10ec95f58278f165c6c0ca64343072bfe4a1
korbin <k1@k1.io>
2014-10-18 13:49:23 -0600

    new: add screenconnect dockerfile/build script

---
+++ screenconnect/screenconnect_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p /tmp/sc/install/
+mkdir -p /opt/screenconnect/
+
+#Untar into install and strip first directory
+tar xzf /tmp/sc/sc.tar.gz --strip-components=1 -C /tmp/sc/install/
+
+#Copy screenconnect runtimes
+cp -r /tmp/sc/install/screenconnect/* /opt/screenconnect/
+
+#Copy bundled mono
+cp /tmp/sc/install/Installer/mono_linux_02_x86_64 /opt/screenconnect/App_Runtime/bin/mono
+chmod 755 /opt/screenconnect/App_Runtime/bin/*
+
+#Run configuration file tranformation
+mono /tmp/sc/install/Installer/Xsl.exe /opt/screenconnect/web.config /tmp/sc/install/Installer/TransformWebConfig.xsl /opt/screenconnect/web.config configuration=Release platform=Mono
